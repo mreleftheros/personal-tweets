f53bb17c91c1d55374d56d24d61e3806cf3c7ed6
eleftheros <eleftheros_all@protonmail.com>
2021-07-01 06:52:56 +0300

    Add addTweet function

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,26 @@
+const tweetsForm = document.getElementById("tweetsForm");
 let tweets;
 
-// check localstorage for tweets item and assign it to tweets variable
+// check localStorage for tweets item and assign it to tweets variable
 const updateFromLocalStorage = () => {
   tweets = (localStorage.tweets) ? JSON.parse(localStorage.getItem("tweets")) : [];
 };
 
+// add new tweet to tweets array
+const addTweet = e => {
+  e.preventDefault(); // prevent default behaviour of form
+  
+  let newTweet = tweetsForm.add.value;
+  if(newTweet) {
+    newTweet = newTweet.trim();
+
+    tweets.push(newTweet); // add to array
+    localStorage.setItem("tweets", JSON.stringify(tweets)) // add to localStorage
+  }
+  
+  e.target.reset();
+}
+
 // events
 window.addEventListener("DOMContentLoaded", updateFromLocalStorage);
+tweetsForm.addEventListener("submit", addTweet);
